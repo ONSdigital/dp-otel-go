4d0ab4d770aa8ce3b66a25e2296a38fe6928e02e
Charles Colbourn <charles.colbourn@ext.ons.gov.uk>
2023-11-01 15:59:40 +0000

    wiping main to allow PR

--- ci/scripts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -eux
-
-cwd=$(pwd)
-
-pushd $cwd/log.go
-  make build
-popd
--- ci/scripts/lint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-cwd=$(pwd)
-
-pushd $cwd/log.go
-# Install golangci-lint
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2
-  make lint
-popd
